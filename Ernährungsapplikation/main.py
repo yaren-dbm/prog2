2db517e3cdbbf3197c713ad6d64c51c36ceecb12
yaren-dbm <7>
2020-06-04 23:51:40 +0200

    Neue Formel

--- Ernährungsapplikation/main.py
+++ Ernährungsapplikation/main.py
@@ -26,11 +26,11 @@ def auswertung():
 
 @app.route('/index.html', methods=['GET', 'POST'])
 
-def index():
+def bmi():
 
     bmi = ''
 
-    if request.method == 'POST' and 'gewicht' in request.form:
+    if request.method == 'POST':
 
         gewicht = float(request.form.get('gewicht'))
 
@@ -44,56 +44,22 @@ def index():
 
 def bmi_rechner(gewicht, grösse):
 
-    return round((gewicht / ((grösse / 100) ** 2)), 2)
+    return round((gewicht / ((grösse / 100) ** 2)), 1)
 
-
-
-
-
-
-@app.route("/speichern/", methods=['GET', 'POST'])
-
-def aktivitaet_speichern():
-
-    if request.method == 'POST':
-
-        aktivitaet = request.form['aktivitaet']
-
-        zeitpunkt, aktivitaet = daten.aktivitaet_speichern(aktivitaet)
-
-        rueckgabe_string = "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
-
-        return rueckgabe_string
-
-
-
-    return render_template("index.html")
-
-
-
-@app.route("/liste")
-
-def auflisten():
-
-    aktivitaeten = daten.aktivitaeten_laden()
-
-
-
-    aktivitaeten_liste = ""
-
-    for key, value in aktivitaeten.items():
-
-        zeile = str(key) + ": " + value + "<br>"
-
-        aktivitaeten_liste += zeile
-
-
-
-    return aktivitaeten_liste
-
-
-
-
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
